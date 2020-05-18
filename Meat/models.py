672ccd0b953eb390604c0c913b318543dbe29a75
akiiita <xhw@akiiita.com>
2020-05-19 00:07:35 +0800

    add Base User Meat

---
+++ Meat/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Meat(models.Model):
+    """
+    肉类（任务）
+    """
+
+    content = models.TextField(
+        default=None,
+        null=True,
+    ),
+    toad = models.ForeignKey(
+        'User.User',
+        related_name='toad_user',
+        on_delete=models.CASCADE,
+    ),
+    target_time = models.DateTimeField(
+        null=True
+    ),
+    eaten = models.BooleanField(
+        default=False
+    )
+
+# Create your models here.
